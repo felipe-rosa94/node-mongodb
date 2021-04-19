c87ffcfae16e3746d5792b816e01814577c0ba34
felipe-rosa94 <luisfeliperc1994@gmail.com>
2021-04-19 10:07:00 -0300

    enviar

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors({origin: '*'}))
 app.use(express.json({type: ['application/json', 'text/plain']}))
 
-const url = 'mongodb://admin:!LiPe0310@whiledev.com.br:27017/whiledev01'
+const url = ''
 mongoose.connect(url, {useNewUrlParser: true})
 mongoose.connection.on('open', () => {
     console.log('conectado')
