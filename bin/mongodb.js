e886c76d18a6a102d1e872372bfb9b97ad6968c1
felipe-rosa94 <luisfeliperc1994@gmail.com>
2021-04-21 11:11:20 -0300

    Variaveis de ambiente

--- bin/mongodb.js
+++ bin/mongodb.js
@@ -1,32 +1,6 @@
 require('dotenv').config()
 const app = require('../src/app')
 const http = require('http')
-
 const httpServer = http.createServer(app)
-
 httpServer.listen(process.env.PORT)
-httpServer.on('error', onError)
-
 console.log('API MongoDB rodando')
-
-function onError(error) {
-    if (error.syscall !== 'listen')
-        throw error
-
-    const bind = typeof port === 'string' ?
-        'Pipe ' + port :
-        'Port ' + port;
-
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requer privilégios elevados')
-            process.exit(1)
-            break
-        case 'EADDRINUSE':
-            console.error(bind + ' já está em uso')
-            process.exit(1)
-            break
-        default:
-            throw error
-    }
-}
